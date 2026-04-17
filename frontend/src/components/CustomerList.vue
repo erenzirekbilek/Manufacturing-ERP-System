e269f2295b5b342c76edca56325ba4f692343f92
Eren Zirekbilek <erenzirekbilek@hotmail.com>
2026-04-17 11:32:45 +0300

    Add CustomerList component

---
+++ frontend/src/components/CustomerList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="customer-list">
+    <table v-if="customers.length">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="customer in customers" :key="customer.id">
+          <td>{{ customer.id }}</td>
+          <td>{{ customer.name }}</td>
+          <td>{{ customer.email }}</td>
+          <td>{{ customer.phone }}</td>
+          <td>
+            <button @click="$emit('edit', customer)">Edit</button>
+            <button @click="$emit('delete', customer.id)">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <p v-else>No customers found.</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({ customers: { type: Array, default: () => [] } });
+defineEmits(['edit', 'delete']);
+</script>
